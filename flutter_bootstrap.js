183e499e0f223ac93f558cb2813836d45480e0b4
Trio Pambudi Utomo <triopambudiutomo@gmail.com>
2025-08-26 23:46:32 +0700

    feat: params in list

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -12,7 +12,7 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 _flutter.loader.load({
   entrypointUrl: "/main.dart.js",
   serviceWorker: {
-    serviceWorkerVersion: "3079357572",
+    serviceWorkerVersion: "3242264752",
     serviceWorkerUrl: "/flutter_service_worker.js?v=",
   },
   onEntrypointLoaded: function(engineInitializer) {
