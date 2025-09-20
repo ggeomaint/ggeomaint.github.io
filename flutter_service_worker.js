4dc77cc2dbe0a3a9a5e5f5c29ac062df0f54c3cb
Trio Pambudi Utomo <triopambudiutomo@gmail.com>
2025-09-20 13:21:31 +0700

    feat: add loading

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "ecdc60a6aa645529ec5eee6e9b0916f6",
+const RESOURCES = {"flutter_bootstrap.js": "0ca11ae6aa9ded32f1dab80954b17e86",
 "version.json": "74d3c9e5aa98d8ae0fe099bb08e6aac2",
 "splash/img/light-2x.png": "714936e60571490cf36c6edbbf771faf",
 "splash/img/dark-4x.png": "b666ce5c6d8198ec8fe597348e320dd2",
@@ -15,7 +15,7 @@ const RESOURCES = {"flutter_bootstrap.js": "ecdc60a6aa645529ec5eee6e9b0916f6",
 "splash/img/light-1x.png": "75bac82831e8bb114506aefddd8527b6",
 "index.html": "a8079d58c209db8a4bdec9be7a8afc93",
 "/": "a8079d58c209db8a4bdec9be7a8afc93",
-"main.dart.js": "40e5bf128d574fd50eeb4856aaf2d4db",
+"main.dart.js": "660ba80110bc6c47de93c6ea3c80ce6b",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "favicon.png": "ef09d158ff24fb749cff11943c8e9073",
 "icons/Icon-192.png": "874892b0e1042a9e7a6a1f087aaa3df1",
