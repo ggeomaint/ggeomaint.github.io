0df26c703c684ba10fdaf2afc0d9220b20580af4
Trio Pambudi Utomo <triopambudiutomo@gmail.com>
2025-09-27 23:18:34 +0700

    feat: improvement ui

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -12,7 +12,7 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 _flutter.loader.load({
   entrypointUrl: "/main.dart.js",
   serviceWorker: {
-    serviceWorkerVersion: "4037547677",
+    serviceWorkerVersion: "3514146263",
     serviceWorkerUrl: "/flutter_service_worker.js?v=",
   },
   onEntrypointLoaded: function(engineInitializer) {
