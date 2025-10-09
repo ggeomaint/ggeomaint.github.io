e3ac8e25ca40b17a92e0542b9233400279587e75
Trio Pambudi Utomo <triopambudiutomo@gmail.com>
2025-10-09 14:43:08 +0700

    fix: button

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -12,7 +12,7 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 _flutter.loader.load({
   entrypointUrl: "/main.dart.js",
   serviceWorker: {
-    serviceWorkerVersion: "2857442331",
+    serviceWorkerVersion: "797033466",
     serviceWorkerUrl: "/flutter_service_worker.js?v=",
   },
   onEntrypointLoaded: function(engineInitializer) {
