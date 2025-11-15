ec0f1886a08c66c3624b539b1e3c68a3bc3e8853
Trio Pambudi Utomo <triopambudiutomo@gmail.com>
2025-11-16 06:13:00 +0700

    fix: validasi serials

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "bc7b9340ecdc26b9be2d43f4a1f70da0",
+const RESOURCES = {"flutter_bootstrap.js": "50495884d9a583fa57b5f32d573b574e",
 "version.json": "74d3c9e5aa98d8ae0fe099bb08e6aac2",
 "splash/img/light-2x.png": "a60f9570da0a666c7313064d201076c0",
 "splash/img/dark-4x.png": "41e517dcd65f960056667304f7815d63",
@@ -15,7 +15,7 @@ const RESOURCES = {"flutter_bootstrap.js": "bc7b9340ecdc26b9be2d43f4a1f70da0",
 "splash/img/light-1x.png": "e95b8e8af6426612141b0ea4bab72ee2",
 "index.html": "9704a7ebe67ab977baa397593b40d08b",
 "/": "9704a7ebe67ab977baa397593b40d08b",
-"main.dart.js": "031f662803f61fc0e77c12d5bd43b314",
+"main.dart.js": "6a4d9274ea00d260c081bcb5d7763634",
 "config.json": "4383f864444c98551a462dee5e07de5e",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "favicon.png": "c0edeb0b945a013a185b6ebb900af6eb",
